196f5d308087b0eb8c8053645a5a2c0657a4a470
divya-raichura <divyaraichura8@gmail.com>
2023-01-29 16:27:01 +0530

    frontend auth and prisma setup

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,27 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+
+export default function Home() {
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <>
+        <img
+          src={session.user?.image as string}
+          alt="Picture of the author"
+          width={50}
+          height={50}
+        />
+        Signed in as {session.user?.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
+
+  return (
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
+  );
+}
