f9c6583770d7297ca1f025d1ec3c59472681f4e2
divya-raichura <divyaraichura8@gmail.com>
2023-04-29 13:37:28 +0530

    package.json update

--- frontend/src/graphql/apollo-client.ts
+++ frontend/src/graphql/apollo-client.ts
@@ -4,8 +4,11 @@ import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
 import { createClient } from "graphql-ws";
 import { getSession } from "next-auth/react";
 
+// backend url
+const DOMAIN = process.env.DOMAIN || "localhost:4000";
+
 const httpLink = new HttpLink({
-  uri: `http://localhost:4000/graphql`,
+  uri: `http://${DOMAIN}/graphql`,
   credentials: "include",
 });
 
@@ -13,7 +16,7 @@ const wsLink =
   typeof window !== "undefined"
     ? new GraphQLWsLink(
         createClient({
-          url: "ws://localhost:4000/graphql/subscriptions",
+          url: `ws://${DOMAIN}/graphql/subscriptions`,
           connectionParams: async () => ({
             session: await getSession(),
           }),
